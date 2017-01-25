858d8e97e7dd7a907dee4d11a5a15fb85942cf0f
Kimberly Pennington <kimberlythegeek@gmail.com>
2017-01-25 11:50:01 -0700

    finished tutorial part 2

--- wikipedia/angular/app.js
+++ wikipedia/angular/app.js
@@ -9,17 +9,26 @@
   function SearchController($scope, $http, API_PATH){
     var _this = this;
     $scope.results = [];
-    
+
     _this.searchTerm = '';
     $scope.$watch('ctrl.searchTerm', function(newVal, oldVal) {
       $.getJSON(API_PATH + '?action=opensearch&callback=?&search='
       + newVal, function(json){
-
         $scope.results = json;
         $scope.$apply();
-        console.log($scope.results);
+        // Log Statement Used Only for Debugging
+        // console.log($scope.results);
       });
     });
+
+    //--- Search Function replaced with $scope.$watch in Part 2 ---//
+    // _this.search = function() {
+    //   $.getJSON(API_PATH + '?action=opensearch&callback=?&search='
+    //   + _this.searchTerm, function(json){
+    //     $scope.results = json;
+    //   });
+    // };
+
   }
-  
+
 })();
